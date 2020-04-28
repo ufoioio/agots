ac3ea4698c48be0390e456ed10698503f1ce6d64
hot_boy <ufoioio@naver.com>
2020-04-28 17:18:10 +0900

    add back, front num parameter for std to multiply setting

--- agots/multivariate_generators/multivariate_extreme_outlier_generator.py
+++ agots/multivariate_generators/multivariate_extreme_outlier_generator.py
@@ -4,9 +4,12 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, factor=8, random=True):
+    def __init__(self, timestamps=None, factor=8, random=True, back=10, front=10):
         self.timestamps = [] if timestamps is None else list(sum(timestamps, ()))
         self.factor = factor
+        self.back = back
+        self.front = front
+
         if random:
             self.updown = np.random.choice([-1, 1])
         else:
@@ -14,7 +17,7 @@ class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
 
     def get_value(self, current_timestamp, timeseries):
         if current_timestamp in self.timestamps:
-            local_std = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].std()
+            local_std = timeseries.iloc[max(0, current_timestamp - self.back):current_timestamp + self.front].std()
             return self.updown * self.factor * local_std
         else:
             return 0
