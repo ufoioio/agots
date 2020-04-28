18f801fa33e1b627b03cd86ca5ed3874b349aea3
hot_boy <ufoioio@naver.com>
2020-04-28 18:55:28 +0900

    remove useless comments

--- agots/multivariate_generators/multivariate_freq_outlier_generator.py
+++ agots/multivariate_generators/multivariate_freq_outlier_generator.py
@@ -16,9 +16,6 @@ class MultivariateFreqOutlierGenerator(MultivariateOutlierGenerator):
     def add_outliers(self, timeseries):
         additional_values = np.zeros(timeseries.size)
         for start, end in self.timestamps:
-            # difference = np.diff(timeseries[start-1:end]) if start > 0 \
-            #              else np.insert(np.diff(timeseries[start:end]), 0, 0)
-
             x = np.array(list(range(start, end)))
 
             additional_values[list(range(start, end))] += self.amplitude * np.cos(2*np.pi*self.freq*x+self.phase_shift)
