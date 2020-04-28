de8267b85e1d81ffd70d8a95a0ef87afbe34faba
hot_boy <ufoioio@naver.com>
2020-04-28 17:10:00 +0900

    can choose up and down by manually by factor

--- agots/multivariate_generators/multivariate_extreme_outlier_generator.py
+++ agots/multivariate_generators/multivariate_extreme_outlier_generator.py
@@ -4,14 +4,18 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, factor=8):
+    def __init__(self, timestamps=None, factor=8, random=True):
         self.timestamps = [] if timestamps is None else list(sum(timestamps, ()))
         self.factor = factor
+        if random:
+            self.updown = np.random.choice([-1, 1])
+        else:
+            self.updown = 1 #following factor
 
     def get_value(self, current_timestamp, timeseries):
         if current_timestamp in self.timestamps:
             local_std = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].std()
-            return np.random.choice([-1, 1]) * self.factor * local_std
+            return self.updown * self.factor * local_std
         else:
             return 0
 
